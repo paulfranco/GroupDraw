a8da5b5e4ff567c9b6037ac3d0021efb64c0c409
Paul Franco <mail@paulfran.co>
2021-10-31 01:25:18 -0700

    setup the tool picker

--- GroupDraw/ViewController.swift
+++ GroupDraw/ViewController.swift
@@ -16,11 +16,35 @@ class ViewController: UIViewController {
         canvasView.translatesAutoresizingMaskIntoConstraints = false
         return canvasView
     }()
+    
+    private lazy var toolPicker: PKToolPicker = {
+        let toolPicker = PKToolPicker()
+        return toolPicker
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupCanvasView()
+        setupToolPicker()
+        
+        
     }
+    
+    private func setupToolPicker() {
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+    }
+    
+    private func setupCanvasView() {
+        view.addSubview(canvasView)
+        // Adding constraints programatically
+        canvasView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        canvasView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        canvasView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        canvasView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true    }
 
 
 }
